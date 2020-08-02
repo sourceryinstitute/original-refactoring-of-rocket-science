706fcff2fac7b9265c4ff9d577ea287efb5662d4
Damian Rouson <damian@sourceryinstitute.org>
2020-08-02 13:43:42 -0700

    add only clause to module use in rocket() function

--- src/rocket/modules/module_variables.f90
+++ src/rocket/modules/module_variables.f90
@@ -2,9 +2,38 @@ module mod1
 use kind_parameters, only : rkind
 implicit none
 save
-real(rkind):: cp,cv,g,rgas,mw,vol,dia,cf,id,od,length,rref,rhos,psipa,pref,db,dt,tmax
+
+  ! define propellent grain geom; simple outward burning cylinder.
+  ! outer diameter is inhibited since this is a cast propellent meaning
+  ! it was poured into the tube/chamber and only the inner diameter
+  ! burns when ignited
+  real(rkind), parameter :: id=0.25_rkind  ! inner diameter of propellant
+  real(rkind), parameter :: od=0.5_rkind  ! outder diameter
+  real(rkind), parameter :: length=1.0_rkind ! propellant grain length
+  real(rkind), parameter :: rref=0.05_rkind  ! propellant burn rate at Pref  (m/s)
+
+  ! gas properties
+  real(rkind), parameter :: mw=28_rkind   ! kg/mol
+  real(rkind), parameter :: rgas=8314._rkind/mw
+
+  ! combustion parameters
+  real(rkind), parameter :: rhos=2000_rkind ! kg/m3
+  real(rkind), parameter :: n=.4 ! burn rate exponent
+  real(rkind), parameter :: psipa=6894.76_rkind ! pascals per psi (constant)
+  real(rkind), parameter :: pref=3000_rkind*psipa ! reference pressure (constant)
+
+  ! boundary condition
+  real(rkind), parameter :: pamb=101325._rkind ! atmospheric pressure
+
+  ! calculate time related values
+  real(rkind), parameter ::  dt=0.001_rkind
+  real(rkind), parameter  ::  tmax=15.0_rkind ! time to stop calculating performance
+
+  real(rkind), parameter :: pi=3.14159_rkind
+
+real(rkind):: cp,cv,g,vol,dia,cf,db
 real(rkind):: thrust, area, r, surf,mdotgen,mdotout,edotgen,edotout,tflame, energy
-real(rkind):: pi, mdotos, edotos, texit, dsigng,pamb,p,t
-real(rkind):: mcham,echam,time,n
+real(rkind):: mdotos, edotos, texit, dsigng,p,t
+real(rkind):: mcham,echam,time
 integer:: nsteps
 end module
--- src/rocket/modules/rocket_function.f90
+++ src/rocket/modules/rocket_function.f90
@@ -1,5 +1,6 @@
 function rocket() result(output)
-use mod1
+use mod1, only : &
+  area, cp, cv, db, dia, dt, echam, g, mcham, nsteps, p, pamb, pi, rgas, t, thrust, time, tmax, vol
 use kind_parameters, only : rkind
 implicit none
 
@@ -11,34 +12,11 @@ real(rkind), allocatable :: output(:,:)
 ! what happens to thrust at low pressures, i.e. shock in the nozzle
 
 
-! define initial variables and constants
-! gas variables
-  cp=1500_rkind ! j/kg/K
-  mw=28_rkind   ! kg/mol
-  tflame=4000._rkind ! Kelvin
-
-! define geometry
+! define initial variables
   vol= 1.0_rkind ! cubic meters
-  dia=0.05_rkind ! meters
-  cf=1.7_rkind ! nozzle thrust coefficient
-
-! define propellent grain geom; simple outward burning cylinder.
-! outer diameter is inhibited since this is a cast propellent meaning
-! it was poured into the tube/chamber and only the inner diameter
-! burns when ignited
-  id=0.25_rkind  ! inner diameter of propellant
-  od=0.5_rkind  ! outder diameter
-  length=1.0_rkind ! propellant grain length
-  rref=0.05_rkind  ! propellant burn rate at Pref  (m/s)
-  rhos=2000_rkind ! kg/m3
-  n=.4 ! burn rate exponent
-  psipa=6894.76_rkind ! pascals per psi (constant)
-  pref=3000_rkind*psipa ! reference pressure (constant)
   db=0_rkind ! initial burn distance
 
 ! calculate time related values
-  dt=0.001_rkind
-  tmax=15.0_rkind ! time to stop calculating performance
   nsteps=nint(tmax/dt) ! number of time steps
 ! preallocate an output file for simulation infomration
   allocate(output(nsteps,4))
@@ -49,29 +27,27 @@ real(rkind), allocatable :: output(:,:)
 
 ! now begin calculating and initializing
 ! gas variables
-  rgas=8314._rkind/mw
   cv=cp-rgas
   g=cp/cv
-  pi=3.14159_rkind
   area=pi/4.0_rkind*dia**2.0_rkind
 
   t=300._rkind ! initial temp (Kelvin)
-  pamb=101325._rkind ! atmospheric pressure
   p=pamb  ! initial chamber pressure
 
 !  calculate initial mass and energy in the chamber
   mcham=p*vol/rgas/t
   echam=mcham*cv*t
   time=0._rkind
+
   do i=1,nsteps
 
    call calmdotgen
-   call massflow
-  ! [mdot,engy,dsign]= massflow(p1,pamb,t1,tamb,cp,cp,rgas,rgas,g,g,area)
+   call massflow   ! [mdot,engy,dsign]= massflow(p1,pamb,t1,tamb,cp,cp,rgas,rgas,g,g,area)
    call addmass
    call calct
    call calcp
    call calcthrust
+
    output(i,:)=[time,p,t, thrust]
    time=time+dt
   enddo
