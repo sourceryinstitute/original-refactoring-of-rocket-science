8cdcddc9c00eb6241459231ad747d27ae46bc7eb
Brad Richardson <everythingfunctional@protonmail.com>
2020-09-23 13:50:35 -0500

    Implement a hard-coded test for nozzle

--- tests/unit/nozzle-class/test-nozzle-class.f90
+++ tests/unit/nozzle-class/test-nozzle-class.f90
@@ -1,11 +1,18 @@
 program main
-  !! Test nozzle abstraction
-  use nozzle_interface, only : nozzle_t
+  use assertions_interface, only: assert
+  use kind_parameters, only: rkind
+  use nozzle_interface, only: nozzle_t
+  use universal_constants, only: PI
+
   implicit none
 
-  type(nozzle_t) nozzle
+  type(nozzle_t) :: nozzle
 
   call nozzle%define("rocket.inp")
+
+  call assert(abs(nozzle%diameter() - 0.05_rkind) < 1.0e-6_rkind, "diameter")
+  call assert(abs(nozzle%area() - 0.05_rkind**2 / 4 * PI) < 1.0e-6_rkind, "area")
+  call assert(abs(nozzle%thrust(1.0_rkind) - 0.05_rkind**2 / 4 * PI * 1.7_rkind) < 1.0e-6_rkind, "thrust")
 
   print *," Test passed."
 
