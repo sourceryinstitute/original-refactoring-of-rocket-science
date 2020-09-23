6a9964319ee3191418f13c5fcdfadb600728470d
Brad Richardson <everythingfunctional@protonmail.com>
2020-09-23 14:03:05 -0500

    parameterize nozzle test over diameter

--- tests/unit/nozzle-class/test-nozzle-class.f90
+++ tests/unit/nozzle-class/test-nozzle-class.f90
@@ -8,11 +8,19 @@ program main
 
   type(nozzle_t) :: nozzle
 
-  nozzle = nozzle_t(diameter = 0.05_rkind, C_f = 1.7_rkind)
+  real(rkind) :: diameter
 
-  call assert(abs(nozzle%diameter() - 0.05_rkind) < 1.0e-6_rkind, "diameter")
-  call assert(abs(nozzle%area() - 0.05_rkind**2 / 4 * PI) < 1.0e-6_rkind, "area")
-  call assert(abs(nozzle%thrust(1.0_rkind) - 0.05_rkind**2 / 4 * PI * 1.7_rkind) < 1.0e-6_rkind, "thrust")
+  call random_number(diameter)
+
+  nozzle = nozzle_t(diameter = diameter, C_f = 1.7_rkind)
+
+  call assert( &
+      abs(nozzle%diameter() - diameter) < 1.0e-6_rkind, &
+      "the given diameter is preserved")
+  call assert( &
+      abs(nozzle%area() - diameter**2 / 4.0_rkind * PI) < 1.0e-6_rkind, &
+      "the area is calculated in accordance with the given diameter")
+  call assert(abs(nozzle%thrust(1.0_rkind) - diameter**2 / 4 * PI * 1.7_rkind) < 1.0e-6_rkind, "thrust")
 
   print *," Test passed."
 
